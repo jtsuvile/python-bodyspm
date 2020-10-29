e9142d8872f4f8e0a0fa6031fccb65a700172607
Juulia Suvilehto <juulia.suvilehto@liu.se>
2020-10-29 17:31:17 +0100

    preprocess without empty on purpose for all controls

--- s0_preprocess_kipu.py
+++ s0_preprocess_kipu.py
@@ -30,14 +30,13 @@ if who == 'control':
     dataloc = '/m/nbe/scratch/socbrain/kipupotilaat/data/controls/subjects/'
     outdataloc = '/m/nbe/scratch/socbrain/kipupotilaat/data/controls/processed/'
     subfile = '/m/nbe/scratch/socbrain/kipupotilaat/data/controls/subs.txt'
-    #subfile = '/m/nbe/scratch/socbrain/kipupotilaat/data/matched_controls_may_2019.txt'
     field_names = [['sex', 'age', 'weight','height','handedness','education','work_physical','work_sitting','profession','psychologist','psychiatrist', 'neurologist'],
                ['pain_now','pain_last_day', 'pain_chronic','hist_migraine','hist_headache','hist_abdomen','hist_back_shoulder','hist_joint_limb','hist_menstrual',
                 'painkillers_overcounter','painkillers_prescription', 'painkillers_othercns'],
                ['feels_pain','feels_depression','feels_anxiety','feels_happy','feels_sad','feels_angry','feels_fear','feels_surprise','feels_disgust'],
                ['bpi_worst', 'bpi_least', 'bpi_average', 'bpi_now', 'bpi_painkiller_relief'],
                ['bpi_functioning', 'bpi_mood','bpi_walk','bpi_work', 'bpi_relationships','bpi_sleep','bpi_enjoyment']]
-    csvname = '/m/nbe/scratch/socbrain/kipupotilaat/data/bg_all_controls_16_10_2020.csv'
+    csvname = '/m/nbe/scratch/socbrain/kipupotilaat/data/bg_all_controls_29_10_2020.csv'
 elif who == 'helsinki':
     dataloc = '/m/nbe/scratch/socbrain/kipupotilaat/data/helsinki/subjects/'
     outdataloc = '/m/nbe/scratch/socbrain/kipupotilaat/data/helsinki/processed/'
@@ -66,8 +65,8 @@ elif who == 'stockholm':
     csvname = '/m/nbe/scratch/socbrain/kipupotilaat/data/bg_pain_stockholm.csv'
 elif who == 'matched_controls_helsinki':
     dataloc = '/m/nbe/scratch/socbrain/kipupotilaat/data/controls/subjects/'
-    outdataloc = '/m/nbe/scratch/socbrain/kipupotilaat/data/controls/processed/'
-    subfile = '/m/nbe/scratch/socbrain/kipupotilaat/data/age_and_gender_matched_subs_pain_helsinki.csv'
+    outdataloc = '/m/nbe/scratch/socbrain/kipupotilaat/data/controls/processed/matched_controls/'
+    subfile = '/m/nbe/scratch/socbrain/kipupotilaat/data/age_and_gender_matched_subs_pain_helsinki_after_qc.csv'
     field_names = [['sex', 'age', 'weight','height','handedness','education','work_physical','work_sitting','profession','psychologist','psychiatrist', 'neurologist'],
                ['pain_now','pain_last_day', 'pain_chronic','hist_migraine','hist_headache','hist_abdomen','hist_back_shoulder','hist_joint_limb','hist_menstrual',
                 'painkillers_overcounter','painkillers_prescription', 'painkillers_othercns'],
@@ -76,7 +75,7 @@ elif who == 'matched_controls_helsinki':
                ['bpi_functioning', 'bpi_mood','bpi_walk','bpi_work', 'bpi_relationships','bpi_sleep','bpi_enjoyment']]
     matchdata = pd.read_csv(subfile)
     subnums = list(matchdata['control_id'])
-    csvname = '/m/nbe/scratch/socbrain/kipupotilaat/data/bg_matched_controls_30_10_2019.csv'
+    csvname = '/m/nbe/scratch/socbrain/kipupotilaat/data/bg_matched_controls_19_10_2020.csv'
 elif who == 'matched_controls_two_each':
     dataloc = '/m/nbe/scratch/socbrain/kipupotilaat/data/controls/subjects/'
     outdataloc = '/m/nbe/scratch/socbrain/kipupotilaat/data/controls/processed/'
@@ -95,38 +94,41 @@ elif who == 'matched_controls_two_each':
     csvname = '/m/nbe/scratch/socbrain/kipupotilaat/data/bg_double_matched_controls_30_10_2019.csv'
 
 
-if who is not 'matched_controls_helsinki' or 'matched_controls_two_each':
+if who is not 'matched_controls_helsinki' and who is not 'matched_controls_two_each':
     with open(subfile) as f:
         subnums = f.readlines()
     subnums = [x.strip() for x in subnums]
 
 # # read subjects from web output and write out to a more sensible format
-# preprocess_subjects(subnums, dataloc, outdataloc, stim, bg_files, field_names)
+if who is 'helsinki' or who is 'stockholm':
+    preprocess_subjects(subnums, dataloc, outdataloc, stim, bg_files, field_names, intentionally_empty=True)
+else:
+    preprocess_subjects(subnums, dataloc, outdataloc, stim, bg_files, field_names)
 
 # # Gather subjects into one dict
 #
 # #grouping = [groupname] * len(subnums)
 #
 #
-# # Combining data (with or without pain information)
-# if who == 'helsinki':# or who == 'stockholm':
-#     with open(grouping_file, newline='', encoding='utf-8-sig') as csvfile:
-#         grouping_data = list(csv.reader(csvfile, delimiter=';'))
-#     group_df = pd.DataFrame(grouping_data)
-#     group_df.columns = group_colnames
-#     # cleaning up fields filled in by humans
-#     group_df['diagnosis'] = group_df['diagnosis'].str.upper()
-#     group_df['diagnosis'] = group_df['diagnosis'].str.replace(' ', '_')
-#     group_df['subid'] = group_df['subid'].str.replace(' ', '')
-#     group_df['subid'] = group_df['subid'].astype(int)
-#     add_background_table(group_df, 'subid', outdataloc, override=True)
-#     subs_with_diagnosis = list(set(group_df['subid'].values) & set(list(map(int, subnums))))
-#     subs_and_diagnoses= group_df[group_df['subid'].isin(subs_with_diagnosis)][['diagnosis','subid']]
-#     full_dataset = combine_data(outdataloc, subs_and_diagnoses['subid'].values, groups=subs_and_diagnoses['diagnosis'].values,
-#                                 save=True, noImages=False)
-# else:
-#    print("combining data from ", len(subnums), " subjects"
-full_dataset = combine_data(outdataloc, subnums,
+# Combining data (with or without pain information)
+if who == 'helsinki':# or who == 'stockholm':
+    with open(grouping_file, newline='', encoding='utf-8-sig') as csvfile:
+        grouping_data = list(csv.reader(csvfile, delimiter=';'))
+    group_df = pd.DataFrame(grouping_data)
+    group_df.columns = group_colnames
+    # cleaning up fields filled in by humans
+    group_df['diagnosis'] = group_df['diagnosis'].str.upper()
+    group_df['diagnosis'] = group_df['diagnosis'].str.replace(' ', '_')
+    group_df['subid'] = group_df['subid'].str.replace(' ', '')
+    group_df['subid'] = group_df['subid'].astype(int)
+    add_background_table(group_df, 'subid', outdataloc, override=True)
+    subs_with_diagnosis = list(set(group_df['subid'].values) & set(list(map(int, subnums))))
+    subs_and_diagnoses= group_df[group_df['subid'].isin(subs_with_diagnosis)][['diagnosis','subid']]
+    full_dataset = combine_data(outdataloc, subs_and_diagnoses['subid'].values, groups=subs_and_diagnoses['diagnosis'].values,
+                                save=True, noImages=False)
+else:
+    print("combining data from ", len(subnums), " subjects")
+    full_dataset = combine_data(outdataloc, subnums,
                                 save=True, noImages=False)
 
 bg = full_dataset['bg']
